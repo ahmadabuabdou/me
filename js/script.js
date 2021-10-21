1f17a780403e580c7d76b190152c5b2fc572a71a
Ahmad Abuabdou <ahmad.a.k.abuabdou@gmail.com>
2021-10-21 14:39:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+/* ------------- light/dark theme ------------ */
+const dayNight = document.querySelector(".day-night");
 
+dayNight.addEventListener("click", () =>{
+	dayNight.querySelector("i").classList.toggle("fa-sun");
+	dayNight.querySelector("i").classList.toggle("fa-moon");
+	document.body.classList.toggle("dark");
+
+});
+
+window.addEventListener("load", () =>{
+	if (document.body.classList.contains("dark")) {
+		dayNight.querySelector("i").classList.add("fa-sun");
+	}
+	else{
+		dayNight.querySelector("i").classList.add("fa-moon");
+	}
+});
 window.addEventListener("load", ()=>{
 	document.querySelector(".main").classList.remove("hidden");
 	document.querySelector(".home-section").classList.add("active");
@@ -133,22 +150,3 @@ function portfoiloItemDetails(portfoiloItem){
 	document.querySelector(".pp-body").innerHTML =
 	portfoiloItem.querySelector(".portfoilo-item-details").innerHTML;
 }
-
-/* ------------- light/dark theme ------------ */
-const dayNight = document.querySelector(".day-night");
-
-dayNight.addEventListener("click", () =>{
-	dayNight.querySelector("i").classList.toggle("fa-sun");
-	dayNight.querySelector("i").classList.toggle("fa-moon");
-	document.body.classList.toggle("dark");
-
-});
-
-window.addEventListener("load", () =>{
-	if (document.body.classList.contains("dark")) {
-		dayNight.querySelector("i").classList.add("fa-sun");
-	}
-	else{
-		dayNight.querySelector("i").classList.add("fa-moon");
-	}
-});
